b0b8897f0152f8589b17591725b2b9bcdcaab20e
unknown <timorust1@gmail.comm>
2022-10-22 21:31:37 +0300

    firestore start

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Navbar } from './components/navbar'
+import { CreatePost } from './pages/create-post/create-post'
+
 import { Login } from './pages/login'
 import { Main } from './pages/main'
 
@@ -13,6 +15,7 @@ function App() {
 				<Routes>
 					<Route path='/' element={<Main />} />
 					<Route path='/login' element={<Login />} />
+					<Route path='/createpost' element={<CreatePost />} />
 				</Routes>
 			</Router>
 		</div>
