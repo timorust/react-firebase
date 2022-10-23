890e52392b579345cd22a047b4cb8951600340d8
unknown <timorust1@gmail.comm>
2022-10-23 13:09:00 +0300

    firebase fix

--- src/pages/create-post/create-form.tsx
+++ src/pages/create-post/create-form.tsx
@@ -4,6 +4,7 @@ import { yupResolver } from '@hookform/resolvers/yup'
 import { addDoc, collection } from 'firebase/firestore'
 import { auth, db } from '../../config/firebase'
 import { useAuthState } from 'react-firebase-hooks/auth'
+import { useNavigate } from 'react-router-dom'
 
 interface CreateFormData {
 	title: string
@@ -12,6 +13,7 @@ interface CreateFormData {
 
 export const CreateForm = () => {
 	const [user] = useAuthState(auth)
+	const navigate = useNavigate()
 
 	const schema = yup.object().shape({
 		title: yup.string().required('You must add a title.'),
@@ -32,8 +34,9 @@ export const CreateForm = () => {
 		await addDoc(postsRef, {
 			...data,
 			username: user?.displayName,
-			id: user?.uid,
+			userId: user?.uid,
 		})
+		navigate('/')
 	}
 
 	return (
@@ -42,7 +45,7 @@ export const CreateForm = () => {
 			<p style={{ color: 'red' }}>{errors.title?.message}</p>
 			<textarea placeholder='Description...' {...register('description')} />
 			<p style={{ color: 'red' }}>{errors.description?.message}</p>
-			<input type='submit' />
+			<input type='submit' className='submitForm' />
 		</form>
 	)
 }
